10e2282bedf37f304dc36179d5c8c73086a142fe
Artur Watanabe <arturwatanabe@gmail.com>
2023-01-23 02:19:31 -0300

    Added reset and custom tip button functions

--- script.js
+++ script.js
@@ -1,27 +1,47 @@
 let totalValue = 0;
-let tipPorcentage = 0;
+let tipPercentage = 0;
 
 const total = document.getElementById("bill");
 const tip = document.getElementById("groupTips");
 const numberOfPeople = document.getElementById("numberOfPeople");
 const tipAmount = document.getElementById("tipAmount");
+const msgError = document.getElementById("msgError");
+const custom = document.getElementById("custom");
+const buttonReset = document.getElementById("buttonReset");
 
-function getTip() {
-  tip.addEventListener("click", function (event) {
-    tipPorcentage = +event.target.value;
-    totalValue = +total.value;
-  });
+function getTip(event) {
+  if (event) {
+    tipPercentage = +event.target.value;
+  } else {
+    tipPercentage = +custom.value;
+  }
 }
 
 function calcValues() {
-  numberOfPeople.addEventListener("change", () => {
-    let tipValue = totalValue * (tipPorcentage / 100);
-    let newTotal = totalValue + tipValue;
-    let tipPerPerson = tipValue / +numberOfPeople.value;
-    tipAmount.innerText = `R$ ${tipPerPerson.toFixed(2)}`;
-    totalPerson.innerText = `R$ ${newTotal.toFixed(2)}`;
-  });
+  totalValue = +total.value;
+  let tipValue = totalValue * (tipPercentage / 100);
+  let newTotal = totalValue + tipValue;
+  let tipPerPerson = tipValue / +numberOfPeople.value;
+  tipAmount.innerText = `R$ ${tipPerPerson.toFixed(2)}`;
+  totalPerson.innerText = `R$ ${newTotal.toFixed(2)}`;
+}
+function validateFields() {}
+
+function resetFields() {
+  tipAmount.innerText = `R$ 00,00`;
+  totalPerson.innerText = `R$ 00,00`;
 }
 
-getTip();
-calcValues();
+tip.addEventListener("click", function (event) {
+  getTip(event);
+});
+numberOfPeople.addEventListener("change", () => {
+  if (!tipPercentage) {
+    getTip();
+  }
+  calcValues();
+});
+
+buttonReset.addEventListener("click", (event) => {
+  resetFields();
+});
